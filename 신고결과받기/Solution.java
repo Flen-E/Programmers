e97e2988e6da0c8ae98cecc65c5bdb5e7b4254d9
Flen-E <cjj642@naver.com>
2023-10-14 15:58:00 +0900

    신고결과성공

--- 신고결과받기/Solution.java
+++ 신고결과받기/Solution.java
@@ -3,49 +3,57 @@ package 신고결과받기;
 import java.util.*;
 
 class Solution {
-    public static int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-        String [] who_reporter = new String[id_list.length]; //나를 신고한 자들을 넣어줄 배열
-        Arrays.fill(who_reporter,""); // 빈칸으로 채워줌
-        for(String letter : report){
-            String [] name =letter.split(" "); //report를 띄어쓰기로 나눠서 신고자와 신고당한자로 나눔
-            String reporter = name[0]; //신고자
-            String attacker = name[1]; //신고당한자
-            for(int i = 0; i < id_list.length; i++){
-                if(id_list[i].equals(attacker)){ //신고당한자랑 id_list사람과 같다면
-                    if(who_reporter[i] == null){ //신고자가 없다면 그냥 바로 신고자 추가
-                        who_reporter[i] += reporter;
-                    }
-                    else if(!who_reporter[i].contains(reporter)){ // 중복이 아닐시 이어서 추가 
-                        who_reporter[i] += " " + reporter;
-                    } 
-                }   
-            }
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int [] answer = new int[id_list.length];
+        //report를 유니크하게 만들어줌 같은 신고를 삭제해준다
+        HashSet<String> uniqueReport = new HashSet<String>(Arrays.asList(report));
+
+        //신고자와 신고 당한자 가질 map을 만들어준다
+        HashMap<String, HashSet<String>> reporterInfoMap = new HashMap<>();
+        //신고당한자와 신고된 수 가질 map을 만들어줍니다
+        HashMap<String, Integer> reportedCountInfoMap = new HashMap<>();
+        
+        for(int i = 0; i < id_list.length; i++){
+            String reporter = id_list[i];
+            reporterInfoMap.put(reporter,new HashSet<>());
         }
-        for(String declar : who_reporter){
-            if(declar != null){ // 신고자들이 없는 것이 아니라면 
-                String [] acc = declar.split(" ");//나를 신고한 사람들
-                if(acc.length >= k){//나를 신고한 사람이 k이상 있으면 신고당함(이제 신고당한 사람이니 여기에 해당하는 신고자들에게 메일을 보내면 됨)
-                    for(int i = 0; i <id_list.length; i++){
-                        for(String accName : acc){ // 신고한 사람들 하나하나 둘러봄
-                            if(id_list[i].equals(accName)) //id_list사람과 신고자들과 같으면 메일이 감
-                                answer[i]++; //메일온 개수만큼 더해줌
-                        }
-                    }
-                } 
+        
+        for(String reporterList : uniqueReport){
+            // 신고자
+            String reporter = reporterList.split(" ")[0];
+            // 신고당한자
+            String attacker = reporterList.split(" ")[1];
+            //신고자들을 찾아 신고당한사람들을 추가해줌
+            reporterInfoMap.get(reporter).add(attacker);
+            //신고당한 사람의 신고횟수를 만들어줌
+            reportedCountInfoMap.put(attacker, reportedCountInfoMap.getOrDefault(attacker, 0) + 1);
+
+        }
+        //reportedList : reportedCountInfoMap에 들어가는 attacker
+        for(String reportedList : reportedCountInfoMap.keySet()){
+            
+            int count = reportedCountInfoMap.get(reportedList); //신고자수를 받아옴 value
+            
+            //k명 이상이면 신고가 되기때문에 메일을 발송 해줘야함
+            if(count >= k){
+                //모든 유저를 둘러봄
+                for(int i = 0; i < id_list.length; i++){
+                  if(reporterInfoMap.containsKey(id_list[i]) && 
+                     reporterInfoMap.get(id_list[i]).contains(reportedList)){
+                      answer[i]++;
+
+                  }
+                        
+                    
+                }
+                
+                
             }
             
         }
         return answer;
+        
+        
     }
-    public static void main(String[] args){
-        String [] id_list = {"ab", "b", "c"};
-        String [] report = {"b ab", "c ab", "c b"};
-        int k = 2;
 
-        int [] answer = solution(id_list,report,k);
-        for(int i = 0; i < answer.length; i++){
-            System.out.print(answer[i]);
-        }
-    }
 }
